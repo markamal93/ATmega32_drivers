d2437ffaab09b90f891f24d0eaf3406a5ece7fb8
markamal <mark.kamal93@gmail.com>
2024-07-29 13:14:47 +0300

    writing lcd code

--- HAL/LCD16x2/LCD16x2.c
+++ HAL/LCD16x2/LCD16x2.c
@@ -28,7 +28,23 @@ void LCD_Init(void){
 	GPIO_SetPinDirection(LCD_CTRL_PORT , LCD_E , GPIO_OUTPUT);
 	GPIO_SetPinDirection(LCD_CTRL_PORT , LCD_RW , GPIO_OUTPUT);
 	GPIO_SetPinDirection(LCD_CTRL_PORT , LCD_RS , GPIO_OUTPUT);
-
+	_delay_ms(20);
+#ifdef LCD_4BIT_MODE
+	LCD_SendCommand(0x02);
+	LCD_SendCommand(0x28);
+	LCD_SendCommand(LCD_CLEAR_SCREEN); /*Clear screen*/
+	LCD_SendCommand(LCD_RETURN_HOME); /*Move to home*/
+	LCD_SendCommand(LCD_SET_ENTRY_MODE|LCD_MOVE_CURSOR_RIGHT); /*Set entry mode*/
+	LCD_SendCommand(LCD_DISPLAY_ON); /*Display On/Off control*/
+	_delay_ms(2);
+#else
+	LCD_SendCommand(LCD_FUNCTION_SET|LCD_8BIT|LCD_4LINES); /*Function Set*/
+	LCD_SendCommand(LCD_CLEAR_SCREEN); /*Clear screen*/
+	LCD_SendCommand(LCD_RETURN_HOME); /*Move to home*/
+	LCD_SendCommand(LCD_SET_ENTRY_MODE|LCD_MOVE_CURSOR_RIGHT); /*Set entry mode*/
+	LCD_SendCommand(LCD_FUNCTION_SET|LCD_8BIT|LCD_4LINES);
+	LCD_SendCommand(LCD_DISPLAY_ON); /*Display On/Off control*/
+#endif
 }
 void LCD_SendCommand(u8){
 
